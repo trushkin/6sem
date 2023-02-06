f4f9f854e218e24dfd0efdd837d3772c21d9de2b
trushkin <89853652+trushkin@users.noreply.github.com>
2023-02-06 21:51:23 +0300

    Delete RIS/labs/lab1_RMI directory

--- RIS/labs/lab1_RMI/server/src/by/bsuir/ConvertToRoman.java
+++
@@ -1,8 +0,0 @@
-package by.bsuir;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ConvertToRoman extends Remote {
-    String toRoman(int number) throws RemoteException;
-}
